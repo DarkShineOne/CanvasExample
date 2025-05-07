0f12fee30c0b371509863ad37ce12fc61d0f88f6
DarkShineOne <51234139+DarkShineOne@users.noreply.github.com>
2025-05-07 22:10:07 +1000

    example

---
+++ script.js
@@ -0,0 +1,72 @@
+const firstCanvas = document.getElementById('first');
+const firstContext = firstCanvas.getContext('2d');
+
+const secondCanvas = document.getElementById('second');
+const secondContext = secondCanvas.getContext('2d');
+
+const spacing = 3;
+const holes = [];
+
+function resizeCanvas() {
+    firstCanvas.width = window.innerWidth;
+    firstCanvas.height = window.innerHeight;
+    secondCanvas.width = window.innerWidth;
+    secondCanvas.height = window.innerHeight;
+    generateHoles();
+    drawBackground();
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+let mouse = {
+    x: 0,
+    y: 0
+};
+
+firstCanvas.addEventListener('mousemove', function (e) {
+    const rect = firstCanvas.getBoundingClientRect();
+    mouse.x = e.clientX - rect.left;
+    mouse.y = e.clientY - rect.top;
+});
+
+
+function generateHoles() {
+    holes.length = 0;
+
+    for (let y = spacing; y < firstCanvas.height; y += spacing) {
+        for (let x = spacing; x < firstCanvas.width; x += spacing) {
+            holes.push({ x: x, y: y, r: 1 });
+        }
+    }
+}
+
+function drawBackground() {
+
+    const gradient = firstContext.createLinearGradient(0, 0, 0, firstCanvas.height);
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(1, 'cyan');
+
+    firstContext.fillStyle = gradient;
+    firstContext.fillRect(0, 0, firstCanvas.width, firstCanvas.height);
+
+    firstContext.globalCompositeOperation = 'destination-out';
+    for (let hole of holes) {
+        firstContext.beginPath();
+        firstContext.arc(hole.x, hole.y, hole.r, 0, Math.PI * 2);
+        firstContext.fill();
+    }
+}
+
+function animate() {
+    secondContext.clearRect(0, 0, secondCanvas.width, secondCanvas.height);
+    secondContext.beginPath();
+    secondContext.arc(mouse.x, mouse.y, 30, 0, Math.PI * 2);
+    secondContext.fillStyle = 'red';
+    secondContext.fill();
+    secondContext.closePath();
+
+    requestAnimationFrame(animate);
+}
+
+animate();
